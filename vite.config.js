9cb378ca2d71db94febec0a67418033158c06541
Anjana <103304121+anjana1916@users.noreply.github.com>
2025-02-24 14:43:51 +1100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/anjana-kr/', // ← Add this line (crucial for GH Pages)
-  build: {
-    outDir: 'dist', // ← Ensure this matches your build folder
-  }
 })
+
