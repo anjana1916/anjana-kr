79d93411c15e51c87bcd2a763191c2079b488081
Anjana <anjanakr1916@gmail.com>
2025-07-28 22:34:09 +1000

    added new dashboard

--- src/sections/Dashboard/Dashboard.jsx
+++ src/sections/Dashboard/Dashboard.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './DashboardStyles.module.css';
 import covidDashboardPdf from '../../assets/dashboards/covid-dashboard.pdf';
+import homelessnessPdf from '../../assets/dashboards/forecast-dashboard.pdf';
 import covidIcon from '../../assets/dashboards/virus.png';
+import forecastIcon from '../../assets/dashboards/forecast.png';
 import tableauIcon from '../../assets/dashboards/almonds.png'; // Add an icon for Tableau
 
 function Dashboards() {
@@ -18,6 +20,13 @@ function Dashboards() {
       description: "Analyzing the impact of climate change on America's largest tree nut industry from 2000-2023.",
       thumbnail: tableauIcon,
       link: "https://public.tableau.com/app/profile/anjana.karathat.rajeev/viz/DroughtandWildfiresCanCaliforniaKeepGrowingtheWorldsAlmonds/Dashboard1" // Replace with your Tableau Public URL
+    },
+    {
+      title: "Predictive Homelessness Forecasting Dashboard",
+      description: "Analyzing Australia's homelessness service coverage gaps and regional growth trends to identify priority intervention areas.",
+      pdfFile: homelessnessPdf,
+      thumbnail: forecastIcon,
+      link: null // No external link for this dashboard
     }
   ];
 
